abc10471d22decc98653858d431e34ebe335c8a9
root <jarrett_jewart@yahoo.com>
2022-08-01 13:46:50 -0700

    Q0

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Uses a REST API to retrieve information,
+for a given employee ID, returns information about
+his/her TODO list progress."""
+from os import sys
+import requests
+
+
+if __name__ == "__main__":
+        user_id = sys.argv[1]
+            id = int(user_id)
+                user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                                                .format(id))
+                    name = user.json().get("name")
+                        todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+                            all = 0
+                                complete = 0
+
+                                    for k in todos.json():
+                                                if k.get('userId') is id:
+                                                                all += 1
+                                                                        if k.get('userId') is id and k.get('completed') is True:
+                                                                                        complete += 1
+
+                                                                                            print("Employee {} is done with tasks({}/{}):".format(name, complete, all))
+
+                                                                                                for k in todos.json():
+                                                                                                            if k.get('userId') is id and k.get('completed') is True:
+                                                                                                                            print("\t {}".format(k.get('title')))
